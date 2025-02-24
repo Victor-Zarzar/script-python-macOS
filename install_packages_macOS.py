e891399acd32e8fd06f3642b6098d3708d27c530
victorzarzar <victorzarzar58@gmail.com>
2025-02-24 19:00:38 -0300

    Update script.

--- install_packages_macOS.py
+++ install_packages_macOS.py
@@ -28,6 +28,7 @@ commands = [
     "git config --global user.email 'victor@example.com'",
     "brew install virtualenv",
     "brew install fastapi",
+    "brew install pyenv",
     "brew install uvicorn",
     "brew install openssh",
     "brew install --cask tunnelblick",
@@ -45,12 +46,10 @@ commands = [
     "brew install --cask zed",
     "brew install --cask google-chrome",
     "brew install --cask omnidisksweeper",
+    "brew install ollama",
     "brew tap leoafarias/fvm" 
     "brew install fvm"
     "mkdir -p ~/Projects",
-    "mkdir -p ~/development",
-    "unzip ~/Downloads/flutter_macos_arm64_3.24.3-stable.zip -d ~/development/",
-    "echo 'export PATH=$HOME/development/flutter/bin:$PATH' >> ~/.zshenv",
     "source ~/.zshenv",
     "sudo sh -c 'xcode-select -s /Applications/Xcode.app/Contents/Developer && xcodebuild -runFirstLaunch'",
     "sudo xcodebuild -license"
