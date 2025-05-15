07a13c05ec573fac81c0fab27973af89dc5bba4c
victorzarzar <victor@example.com>
2025-05-15 13:25:43 -0300

    Update script

--- install_packages_macOS.py
+++ install_packages_macOS.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 def run_command(command, use_sudo=False):
     if use_sudo:
@@ -9,38 +10,76 @@ def run_command(command, use_sudo=False):
     except subprocess.CalledProcessError as e:
         print(f"❌ Erro ao executar {command}: {e}")
 
+def setup_homebrew():
+    homebrew_check_cmd = '/opt/homebrew/bin/brew --version'
+    try:
+        subprocess.run(homebrew_check_cmd, shell=True, check=True)
+        print("✅ Homebrew já instalado.")
+    except subprocess.CalledProcessError:
+        run_command('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
+     
+        shell_config = os.path.expanduser('~/.zprofile')
+        with open(shell_config, 'a') as file:
+            file.write('\n# Homebrew\n')
+            file.write('eval "$(/opt/homebrew/bin/brew shellenv)"\n')
+        subprocess.run('eval "$(/opt/homebrew/bin/brew shellenv)"', shell=True)
+        print("✅ Homebrew instalado e configurado.")
+
+setup_homebrew()
+
 commands = [
-    'mkdir -p ~/Projects',
-    '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"',
-    '(echo; echo \'eval "$(/opt/homebrew/bin/brew shellenv)"\') >> ~/.zprofile && eval "$(/opt/homebrew/bin/brew shellenv)"',
+    # Fontes e visual
     "brew install --cask font-jetbrains-mono-nerd-font",
+
+    # Terminal
     "brew install starship",
     "brew install zsh-autosuggestions",
+    "brew install fzf",
     "brew install eza",
     "brew install zoxide",
     "brew install bat",
+
+    # Banco de dados e backend
     "brew install sqlite",
     "brew install mysql",
-    "brew install --cask localsend",
-    "brew install --cask keepassxc",
-    "brew install --cask telegram",
-    "brew install cocoapods",
+    "brew install redis",
+    "brew install nginx",
+
+    # Python e ambiente
+    "brew install pyenv",
+    "brew install virtualenv",
+    "brew install openssh",
+    "brew install fastapi",
+    "brew install uvicorn",
+
+    # Node & Dev
     "brew install node",
     "brew install pnpm",
     "brew install git",
+
+    # Git config
     "git config --global user.name 'victorzarzar'",
     "git config --global user.email 'victor@example.com'",
-    "brew install virtualenv",
-    "brew install fastapi",
-    "brew install pyenv",
-    "brew install uvicorn",
-    "brew install openssh",
-    "brew install --cask tunnelblick",
+
+    # Java
     "brew install openjdk@21",
+
+    # Apple e Xcode
+    "sudo sh -c 'xcode-select -s /Applications/Xcode.app/Contents/Developer && xcodebuild -runFirstLaunch'",
+    "sudo xcodebuild -license",
+    "softwareupdate --install-rosetta --agree-to-license",
+
+    # FVM e Flutter
+    "brew tap leoafarias/fvm",
+    "brew install fvm",
+
+    # Apps essenciais
+    "brew install --cask localsend",
+    "brew install --cask keepassxc",
+    "brew install --cask telegram",
+    "brew install --cask tunnelblick",
     "brew install --cask orion",
-    "brew install --cask orbstack",
-    "brew install nginx",
-    "brew install redis",
+
     "brew install --cask sublime-text",
     "brew install --cask visual-studio-code",
     "brew install --cask android-studio",
@@ -52,10 +91,13 @@ commands = [
     "brew install --cask google-drive",
     "brew install --cask omnidisksweeper",
     "brew install --cask protonvpn",
-    "sudo sh -c 'xcode-select -s /Applications/Xcode.app/Contents/Developer && xcodebuild -runFirstLaunch'",
-    "sudo xcodebuild -license",
-    "brew tap leoafarias/fvm",  
-    "brew install fvm"
+    "brew install --cask macs-fan-control",
+
+    # GitHub CLI
+    "brew install gh",
+
+    # Docker
+    "brew install --cask orbstack",
 ]
 
 for cmd in commands:
